394a61ce0ffc5aad23334749cdfa8e450058a42c
LuisGustav18 <luis0gustavo43@gmail.com>
2026-01-05 20:10:43 -0300

    endpoint findById para tecnico

--- src/main/java/com/luis/helpdesk/services/TecnicoService.java
+++ src/main/java/com/luis/helpdesk/services/TecnicoService.java
@@ -1,4 +1,20 @@
 package com.luis.helpdesk.services;
 
+import com.luis.helpdesk.domain.Tecnico;
+import com.luis.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
 }
