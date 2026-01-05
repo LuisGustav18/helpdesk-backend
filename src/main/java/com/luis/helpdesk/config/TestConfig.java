0bbcceb1017c4fcc0f3449c6ea5c6158b1e6a4d7
LuisGustav18 <luis0gustavo43@gmail.com>
2026-01-05 16:07:14 -0300

    perfil de teste criado

---
+++ src/main/java/com/luis/helpdesk/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.luis.helpdesk.config;
+
+import com.luis.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public CommandLineRunner instanciaDB(){
+        return args -> this.dbService.instanciaDB();
+    }
+}
