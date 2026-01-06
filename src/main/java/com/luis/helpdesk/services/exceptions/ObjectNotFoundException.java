c8059da90e12b1567af7ea5d8b6ca73a2f4010d4
LuisGustav18 <luis0gustavo43@gmail.com>
2026-01-06 16:20:26 -0300

    execão para objetos não encontrado

---
+++ src/main/java/com/luis/helpdesk/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.luis.helpdesk.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg, Throwable cause){
+        super(msg, cause);
+    }
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
