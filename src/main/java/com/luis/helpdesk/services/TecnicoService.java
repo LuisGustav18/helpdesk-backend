a0a4560e5c9aa08c9605a17e45105eed8f941b43
LuisGustav18 <luis0gustavo43@gmail.com>
2026-01-06 18:10:37 -0300

    endpoint create para tecnico

--- src/main/java/com/luis/helpdesk/services/TecnicoService.java
+++ src/main/java/com/luis/helpdesk/services/TecnicoService.java
@@ -1,6 +1,7 @@
 package com.luis.helpdesk.services;
 
 import com.luis.helpdesk.domain.Tecnico;
+import com.luis.helpdesk.domain.dtos.TecnicoDTO;
 import com.luis.helpdesk.repositories.TecnicoRepository;
 import com.luis.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,11 @@ public class TecnicoService {
 
     public List<Tecnico> findAll(){
         return repository.findAll();
+    }
+
+    public Tecnico create(TecnicoDTO objDTO){
+        objDTO.setId(null);
+        Tecnico newObj = new Tecnico(objDTO);
+        return repository.save(newObj);
     }
 }
