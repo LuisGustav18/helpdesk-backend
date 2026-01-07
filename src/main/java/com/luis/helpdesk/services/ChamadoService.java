cfc241e1fea1c8f9aa32887d6acce27739972d96
LuisGustav18 <luis0gustavo43@gmail.com>
2026-01-07 14:23:33 -0300

    findById para chamado

---
+++ src/main/java/com/luis/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.luis.helpdesk.services;
+
+import com.luis.helpdesk.domain.Chamado;
+import com.luis.helpdesk.repositories.ChamadoRepository;
+import com.luis.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}
